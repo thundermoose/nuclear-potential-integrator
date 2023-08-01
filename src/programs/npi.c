26fe61317e968f0902978af62c97747f5c972e88
Thundermoose <tordjarv@gmail.com>
2023-08-01 07:38:14 -0400

    Added a basic main file

---
+++ src/programs/npi.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <thundertester/test.h>
+
+int main()
+{
+	printf("Hello world\n");
+	return 0;
+}
+
+new_test(test_of_testing,
+	 {
+	 	printf("All is well\n");
+	 })
